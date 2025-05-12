a68656207e961741d85667e9fd06aecedacaaf9b
job <oberbergerhannes@gmail.com>
2025-05-12 14:39:09 +0200

    ISSUE5 | refactor to use function instead of inline return

--- frontend/dodecaphony-ui/src/components/Header.jsx
+++ frontend/dodecaphony-ui/src/components/Header.jsx
@@ -3,7 +3,6 @@ import {AppBar, Button, Toolbar} from '@mui/material';
 
 function Header() {
     return (
-
         <AppBar position="static" color="success">
             <Toolbar sx={{justifyContent: 'space-between'}}>
                 <Button color="inherit">Dodecaphony generator</Button>
--- frontend/dodecaphony-ui/src/components/PieceDisplay.jsx
+++ frontend/dodecaphony-ui/src/components/PieceDisplay.jsx
@@ -3,7 +3,6 @@ import {Box, CircularProgress} from '@mui/material';
 
 function PieceDisplay() {
     return (
-
         <Box display="flex" justifyContent="center" my={4}>
             <Box width={300} height={200} display="flex" alignItems="center" justifyContent="center" bgcolor="#ddd">
                 <CircularProgress color="secondary"/>
