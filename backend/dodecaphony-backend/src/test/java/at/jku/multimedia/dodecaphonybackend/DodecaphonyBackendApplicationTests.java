6e1f090a4f39452330b642446c622ac9b7c25857
RathbergerJakob <110130607+RathbergerJakob@users.noreply.github.com>
2025-05-23 14:06:45 +0200

    #3 fix broken backend directory

---
+++ backend/dodecaphony-backend/src/test/java/at/jku/multimedia/dodecaphonybackend/DodecaphonyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.jku.multimedia.dodecaphonybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodecaphonyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
